4a515b3b9c9d6a50ba3b03274290d0019f027da2
Chedly Zouche <48986303+Zedo9899@users.noreply.github.com>
2020-11-14 19:45:20 +0100

    TP 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'training-app';
+  //session_name = 'Formation Web';
+  nbpart: number = 0;
+  firstSession = {
+    id: 1,
+    name: 'Formation Web',
+    track: 'MEAN Stack',
+    date: 'Prévue du 11/06/2018 au 15/06/2018',
+    local: 'Lyon',
+    particpants : 0
+  }
+  nbrParticipantsChange(event) {
+    this.nbpart = event.value;
+  }
 }
